49fe46ce6f196db9e6720286714e8acb4f7e3df7
Michael McCouman jr. <support@wikibyte.org>
2013-05-22 21:27:55 +0300

    infos ok
    zusatzinfos zu den Testimplementierungen

--- Podlove-Standalone-Ajax-test/ajax.php
+++ Podlove-Standalone-Ajax-test/ajax.php
@@ -5,7 +5,7 @@
 *
 *	@Developer: Michael McCouman jr.
 *	@Mail: Support@wikibyte.org
-* 	@Version: 0.1.a
+* 	@Version: 0.1.1a
 *
 */
 ################################################################ Podcast Informations über Callback: callback
@@ -18,20 +18,44 @@ if (isset($_GET["callback"]) && !empty($_GET["callback"])) {
   $callback = $_GET["callback"];
 
 
-	// Header für ein JavaScript
+	// Headercall für ein JavaScript output
   	header("Content-Type: application/javascript");
 
 
-/** Chapter Marks Callback (nur für Shordcode in WordPress nutzen) **/
-#$datei = file("http://testpreview.reliveradio.de/stream/technique.chapters");
-#$datei = file("http://cm.wikibyte.org/testcodes/neu-chapters/podlove-web-player/relive.datei");
-  
+/** 
+ * Chapter Callback (nur für Shordcode in WordPress nutzen - für spätere implementierungen!)
+ * #$datei = file("http://testpreview.reliveradio.de/stream/technique.chapters");			//original Relive
+ * #$datei = file("http://cm.wikibyte.org/testcodes/neu-chapters/podlove-web-player/relive.datei"); 	//testfile
+**/
   	echo $callback. "('";
 
-//Tests 
-// so muss die Ausgabe für den Standalone Player aussehen
-echo '<tr class="chaptertr active loaded" data-start="0.5" data-end="1.5" data-img="..."><td class="starttime"><span>00:01</span></td><td class="chaptername">Chapter Two</td><td class="timecode"><span>0:01</span></td></tr>';
-echo '<tr class="chaptertr oddchapter loaded" data-start="0.5" data-end="1.5" data-img="..."><td class="starttime"><span>00:01</span></td><td class="chaptername">Chapter Two</td><td class="timecode"><span>0:01</span></td></tr>';
+	#### Tests - so muss die Ausgabe für den Standalone Player aussehen
+	#
+	# first (aktiv chapter) - läuft gerade
+	echo '<tr class="chaptertr active loaded" data-start="0.5" data-end="1.5" data-img="..."><td class="starttime"><span>00:01</span></td><td class="chaptername">Chapter Two</td><td class="timecode"><span>0:01</span></td></tr>';
+	/**
+	 *   Laufende Episode:
+	 * 
+	 * <tr class="chaptertr active loaded" data-start="0.5" data-end="1.5" data-img="...">
+	 * <td class="starttime"><span>00:01</span></td>
+	 * <td class="chaptername">Chapter Two</td>
+	 * <td class="timecode"><span>0:01</span></td></tr>
+	 */
+	
+	#
+	#
+	# other (odd chapter for the relive list) - was noch kommt                                                                            
+	echo '<tr class="chaptertr oddchapter loaded" data-start="0.5" data-end="1.5" data-img="..."><td class="starttime"><span>00:01</span></td><td class="chaptername">Chapter Two</td><td class="timecode"><span>0:01</span></td></tr>';
+	
+	
+	/**
+	 *  Liste andere:
+	 * 
+	 * <tr class="chaptertr oddchapter loaded" data-start="0.5" data-end="1.5" data-img="...">
+	 * <td class="starttime"><span>00:01</span></td>
+	 * <td class="chaptername">Chapter Two</td>
+	 * <td class="timecode"><span>0:01</span></td></tr>
+	 */
 	
 	//decode Json Schleife für weitere Beiträge- kommt später
 	#foreach($datei AS $meine_datei){ echo $meine_datei; }
@@ -48,13 +72,12 @@ if (isset($_GET["infodown"]) && !empty($_GET["infodown"])) {
   $infodown = $_GET["infodown"];
 
 
-	// Header für ein JavaScript
+	// Headeroutput nach Callback2
   	header("Content-Type: application/javascript");
 
-
-
 	echo $infodown. "('";
-	//so muss die Ausgabe für die Podcastcover & Infos aussehen (#test 12 - modif.:3 im PWP.js file)
+	### so muss die Ausgabe für die Podcastcover & Infos aussehen (#test 12 - modif.:3 im PWP.js file)
+	#
 	echo '<div id="alles" class="podlovewebplayer_meta"><a class="bigplay" title="Play Episode" href="#"></a><div class="coverart"><img class="coverimg" src="http://static.reliveradio.de/logos/savoirvivre.jpg" data-img="http://static.reliveradio.de/logos/brainshares.jpg" alt=""></div><h3 class="episodetitle"><a href="...">LALAL</a></h3><div class="subtitle">...</div><div class="togglers"><a href="#" class="infowindow infobuttons pwp-icon-info-circle" title="More information about this"></a><a href="#" class="chaptertoggle infobuttons pwp-icon-list-bullet" title="Show/hide chapters"></a><a href="#" class="showdownloadbuttons infobuttons pwp-icon-download" title="Show/hide download bar"></a></div>';
 
 	echo "')";
