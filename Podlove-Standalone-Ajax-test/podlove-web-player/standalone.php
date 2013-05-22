9b0c37655a9df7dd5e2416a7c573610a3ac3089c
Michael McCouman jr. <support@wikibyte.org>
2013-05-22 21:41:12 +0300

    Update standalone.php

--- Podlove-Standalone-Ajax-test/podlove-web-player/standalone.php
+++ Podlove-Standalone-Ajax-test/podlove-web-player/standalone.php
@@ -18,13 +18,22 @@ height: auto!important;
 
 
 <script>
+/**
+*	Chapter PWP JS
+*	@dev: Simon Waldherr
+*/
+
 //this is a "template" for each chapter row
 var rowDummy = $('<tr class="chaptertr" data-start="" data-end="" data-img=""><td class="starttime"><span></span></td><td class="chaptername"></td><td class="timecode">\n<span></span>\n</td>\n</tr>');
 
-
-setInterval( function jsonp(){
-
-  // Löschen bereits vorhandener JSONP Skripte
+/**
+*	Listening JS, new, comming
+*	@dev: Michael McCouman jr.
+*	@js: callback 1 
+*	@call: callback
+*	@id: jsonp_antwort
+*/
+setInterval(function jsonp(){
   var scripts = document.getElementsByTagName("script");
   for (i=0; i<scripts.length; i++) {
     var url = scripts[i].getAttribute("src");
@@ -33,8 +42,6 @@ setInterval( function jsonp(){
       scripts[i].parentNode.removeChild(scripts[i]);
     }
   }
-
-  // Anlegen und Einfügen des neuen Skripts
   var now = new Date();
   url = "http://cm.wikibyte.org/testcodes/neu-chapters/ajax.php?time="+now.getTime()+"&callback=callback";
   var script = document.createElement("script");
@@ -43,24 +50,18 @@ setInterval( function jsonp(){
   document.getElementsByTagName("head")[0].appendChild(script);
   
 }, 10500);
-
-//---------------------------- Entgegennahme der Serverantwort
 function callback(data) { 
   document.getElementById("jsonp_antwort").innerHTML = data;
 }
 
-
-
-
-
-
-
-
-
-
-setInterval( function jsonps(){
-
-  // Löschen bereits vorhandener JSONP Skripte
+/**
+*	Cover, Description, Podcastname
+*	@dev: Michael McCouman jr.
+*	@js: callback 2 
+*	@call: infodown
+*	@id: alle
+*/
+setInterval(function jsonps(){
   var scruppts = document.getElementsByTagName("script");
   for (i=0; i<scruppts.length; i++) {
     var url = scruppts[i].getAttribute("src");
@@ -69,8 +70,6 @@ setInterval( function jsonps(){
       scruppts[i].parentNode.removeChild(scruppts[i]);
     }
   }
-
-  // Anlegen und Einfügen des neuen Skripts
   var nieee = new Date();
   url = "http://cm.wikibyte.org/testcodes/neu-chapters/ajax.php?time="+nieee.getTime()+"&infodown=infodown";
   var infoddd = document.createElement("script");
@@ -79,10 +78,7 @@ setInterval( function jsonps(){
   document.getElementsByTagName("head")[0].appendChild(infoddd);
   
 }, 10500);
-
-//---------------------------- Entgegennahme der Serverantwort
 function infodown(data) { 
-  /*document.getElementById("jsonp_antwort").innerHTML = data;*/
   document.getElementById("alles").innerHTML = data;
 }
 </script>
@@ -96,32 +92,30 @@ function infodown(data) {
 
 		<script>
 			$('#testplayer').podlovewebplayer({
-				poster: 'samples/coverimage.png',
-				title: 'PWP001 – Lorem ipsum dolor sit amet',
 				permalink: '...',
-				subtitle: '...',
-				chapters: [{'start':'00:00:00.000','title':'a',   'image':''}
-									,{'start':'00:00:10.500','title':'Chapter Two',   'image':'samples/coverimage-red.png'}
-									,{'start':'00:00:20.500','title':'Chapter Three', 'image':'http://static.reliveradio.de/logos/brainshares.jpg'}
-									,{'start':'00:00:30.000','title':'Chapter Four',  'image':'samples/coverimage-blue.png'}],
-				summary: '<p>Summary and even links <a href="http://testpreview.reliveradio.de/stream/technique.chapters">Capter</a></p>',
-				duration: '24:00.000',
 				alwaysShowHours: true,
 				startVolume: 0.8,
 				width: 'auto',
 				summaryVisible: false,
-				chaptersVisible: true	
+				chaptersVisible: true,
+				summary: '<p>Summary and even links <a href="http://testpreview.reliveradio.de/stream/technique.chapters">Capter</a></p>',
+			
+				
+/*ersetzen nach call2*/		poster: 'samples/coverimage.png',
+/*ersetzen nach call2*/		title: 'PWP001 – Lorem ipsum dolor sit amet',
+/*ersetzen nach call2*/		subtitle: '...',
+				
+				
+/*ersetzen nach call1*/		chapters: [{'start':'00:00:00.000','title':'a',   'image':''}
+,{'start':'00:00:10.500','title':'Chapter Two',   'image':'samples/coverimage-red.png'}
+,{'start':'00:00:20.500','title':'Chapter Three', 'image':'http://static.reliveradio.de/logos/brainshares.jpg'}
+,{'start':'00:00:30.000','title':'Chapter Four',  'image':'samples/coverimage-blue.png'}],
+				
+				duration: '24:00.000' //Gesammtzeit 
+
 			});
 		</script>
 	</p>
-	
-	
-	
-	<div id="jsonp_antwort">test1</div>
-		
-		
-		
-		
-		http://testpreview.reliveradio.de/stream/technique.chapters
+
 </body>
 </html>
