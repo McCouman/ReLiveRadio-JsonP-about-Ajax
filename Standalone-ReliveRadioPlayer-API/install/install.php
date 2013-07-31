7f64cff4d6bc12038e5939f75cbd7e45218adcce
McCouman <support@wikibyte.org>
2013-07-31 19:17:01 +0200

    install standards
    noch vergessen :(

--- Standalone-ReliveRadioPlayer-API/install/install.php
+++ Standalone-ReliveRadioPlayer-API/install/install.php
@@ -1,25 +1,37 @@
 <?php
 /**
 *	Install Webplayer
+*	--------------------------------
+*	Parameter Angaben hier anpassen:
 *
-**/
-#global $setup_url;
+*/
 
-//install URI
+# Install Pfad des gesammten Packetes (URL)
 $setup_url = "http://cm.wikibyte.org/testcodes/neu-chapters";
 
 
-//Standrad Listenanzahl festelegen
-$anzahl = 6;  
-
-//Callback für JS in ms
-#$setup_ms_callback = 60999;
-$setup_ms_callback = 7999; //test
+# Standrad Listenanzahl festelegen
+$anzahl = 5;  											//Standard ist: 5
 
 
-$dec_max_limet = 10; //Textausgabe der Description
+# Callbacks für JS in /ms 
+$setup_ms_callback = 8999; 								//Standard ist: 60999
 
-//JS Data URLs
+
+# Anzahl der Textausgabe: (max 29 Wörter)
+$dec_max_limet = 28;									//Description zur laufenden Sendung im Player ausgeben
+
+
+
+
+
+######################################## Ab hier nicht ändern bitte! #########################################
+/**
+*	Dieser Bereich wird später zu einer Datei zusammengebunden!
+*	Bitte hier nix ändern - danke :)
+**/
+
+#JS Data URLs
 $js_mix_ajax_link = $setup_url. "/ajax-mix.php"; 		//JS - Mix
 $js_ajax_link_tec = $setup_url. "/ajax-technik.php"; 	//JS - Technik
 $js_ajax_link_kul = $setup_url. "/ajax-kultur.php"; 	//JS - Kultur
